fb02f6958bb2f9514330db6c76515ea47c5a423b
LaLunaInSky <moonaflordapele@gmail.com>
2024-11-10 17:35:12 -0300

    10/11/2024 17:34

--- Estudos_Iniciais_Com_o_Curso_Em_Vídeo/Módulo_4/Desafios/d_016/script.js
+++ Estudos_Iniciais_Com_o_Curso_Em_Vídeo/Módulo_4/Desafios/d_016/script.js
@@ -1,5 +1,5 @@
 let logos = ['imagens/logo-home.jpg', 'imagens/logo-youtube.jpg', 'imagens/logo-github.jpg', 'imagens/logo-instagram.jpg', 'imagens/logo-twitter.jpg', 'imagens/logo-facebook.jpg']
-let telas = ['imagens/tela-home.jpg', 'imagens/tela-youtube.jpg', 'imagens/tela-github.jpg', 'imagens/tela-instagram.jpg', 'imagens/tela-twitter.jpg', 'imagens/tela-facebook.jpg']
+let telas = ['imagens/tela-home.jpg', 'imagens/tela-youtube.png', 'imagens/tela-github.jpg', 'imagens/tela-instagram.jpg', 'imagens/tela-twitter.jpg', 'imagens/tela-facebook.jpg']
 
 function criaçãoBotões() {
     let articleMenu = document.querySelector('article.menu')
@@ -7,13 +7,17 @@ function criaçãoBotões() {
         let imgBotão = document.createElement('a')
         imgBotão.setAttribute('href', '#')
         imgBotão.setAttribute('target', '_self')
+        imgBotão.setAttribute('onclick', `escolhaTela(${item})`)
         imgBotão.innerHTML = `<img src="${logos[item]}" alt="">`
         articleMenu.append(imgBotão)
     }
 }
 
 function escolhaTela(num) {
-
+    let item = Number(num)
+    let iframeTela = document.querySelector('iframe')
+    iframeTela.style.background = `transparent url('${telas[item]}') no-repeat`;
+    iframeTela.style.backgroundSize = 'cover';
 }
 
 criaçãoBotões()
