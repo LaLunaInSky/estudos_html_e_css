370321d4a7eece8d6618619c72a5ffe283818683
LaLunaInSky <moonaflordapele@gmail.com>
2024-11-11 08:28:38 -0300

    11/11/2024 08:28

--- Estudos_Iniciais_Com_o_Curso_Em_Vídeo/Módulo_4/Desafios/d_016/script.js
+++ Estudos_Iniciais_Com_o_Curso_Em_Vídeo/Módulo_4/Desafios/d_016/script.js
@@ -1,23 +1,15 @@
 let logos = ['imagens/logo-home.jpg', 'imagens/logo-youtube.jpg', 'imagens/logo-github.jpg', 'imagens/logo-instagram.jpg', 'imagens/logo-twitter.jpg', 'imagens/logo-facebook.jpg']
-let telas = ['imagens/tela-home.jpg', 'imagens/tela-youtube.png', 'imagens/tela-github.jpg', 'imagens/tela-instagram.jpg', 'imagens/tela-twitter.jpg', 'imagens/tela-facebook.jpg']
 
 function criaçãoBotões() {
     let articleMenu = document.querySelector('article.menu')
     for (item in logos) {
         let imgBotão = document.createElement('a')
-        imgBotão.setAttribute('href', '#')
-        imgBotão.setAttribute('target', '_self')
+        imgBotão.setAttribute('href', 'telas.html')
+        imgBotão.setAttribute('target', 'tela')
         imgBotão.setAttribute('onclick', `escolhaTela(${item})`)
         imgBotão.innerHTML = `<img src="${logos[item]}" alt="">`
         articleMenu.append(imgBotão)
     }
-}
-
-function escolhaTela(num) {
-    let item = Number(num)
-    let iframeTela = document.querySelector('iframe')
-    iframeTela.style.background = `transparent url('${telas[item]}') top center no-repeat`;
-    iframeTela.style.backgroundSize = 'cover';
 }
 
 criaçãoBotões()
---
+++ Estudos_Iniciais_Com_o_Curso_Em_Vídeo/Módulo_4/Desafios/d_016/script_telas.js
@@ -0,0 +1,15 @@
+let telas = ['imagens/tela-home.jpg', 'imagens/tela-youtube.png', 'imagens/tela-github.jpg', 'imagens/tela-instagram.jpg', 'imagens/tela-twitter.jpg', 'imagens/tela-facebook.jpg']
+let cores = ['#', '#FE0000', '#148630', '#DB1E63', '#4EA9E5', '#44619C']
+let links = ['#', 'https://www.youtube.com/c/CursoemV%C3%ADdeo/playlists', 'https://github.com/gustavoguanabara', 'https://www.instagram.com/gustavoguanabara/', 'https://x.com/guanabara', 'https://www.facebook.com/guanabara']
+
+
+function escolhaTela(num=0) {
+    let item = Number(num)
+    let tela = document.querySelector('section#telaAtual')
+    tela.innerHTML = `<img src="${telas[item]}" alt="">`
+    if (item != 0) {
+        tela.innerHTML += `<a style="background-color: ${cores[item]};" href="${links[item]}" target="_blank">ACESSAR</a>`
+    }
+}
+
+escolhaTela(2)
